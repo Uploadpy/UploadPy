76630ec1f879fd7b5fd919e3d87960a6fc16cc55
UploadPy <uploadpy@gmail.com>
2020-01-09 22:39:48 +0100

    Added a basic variables and data file.

---
+++ variables.py
@@ -0,0 +1,8 @@
+"""A dictionary of hardcoded variables, some needed, some examples."""
+VARIABLES = {"urls": {"imgur": "https://imgur.com/upload",
+                      "pastebin": "https://upload.pastebin.com",
+                      "minipaint": "http://image.minipaint.com/share"},
+             "exampledata": [b'65b65b8b968cf265cefbf4c172193ed7',
+                             'ecÕ©\x9d\x91¡Æ\x98¨«Ä',
+                             'ecÕ©\x9d\x91\x9bÑ§\x9c¡Ê',
+                             '¨\x9aÃ\x9abÅ§Æ\x9ec\x9aÈÌ¡¨\x9a\x90ÞÕ×\x93©Ö\x96d\x9b¥ZaÈÓ¤']}
